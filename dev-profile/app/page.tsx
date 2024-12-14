0b40904f08c75eccb624439c96dc8652e4365b8f
dev-kweku <frikasoidfrimpong@gmail.com>
2024-12-14 11:23:04 +0000

    fav prohect added

--- dev-profile/app/page.tsx
+++ dev-profile/app/page.tsx
@@ -1,11 +1,15 @@
 import { Hero } from "./components/Hero";
 import { SectionTwo } from "./components/SectionTwo";
+import { Footer } from "./components/Footer";
+import { AboutSection } from "./components/AboutSection";
 
 export default function Home() {
   return (
     <div className="max-w-7xl w-full px-4 md:px-8 mx-auto">
       <Hero/>
       <SectionTwo/>
+      <AboutSection/>
+      <Footer/>
 
     </div>
   );
